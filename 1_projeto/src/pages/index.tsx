8e8debe4bcf4df2cd1bae25b7fc5e769d13cbf29
João Claudio Prestes <119965861+joaoclaudioprestes@users.noreply.github.com>
2024-02-03 22:13:36 -0300

    Update index.tsx

--- 1_projeto/src/pages/index.tsx
+++ 1_projeto/src/pages/index.tsx
@@ -1,11 +1,32 @@
+import { AnyAaaaRecord } from "dns";
+
 // Global
 const name: string = "João C. Prestes";
 const myJob: string = "Developer";
+const myAge: number = 19;
+
+function helloWorld(color: boolean) {
+  let textColor: boolean = color;
+
+  return (
+    <>
+      {!textColor ? (
+        <h2 className="text-[30px]">Hello Next</h2>
+      ) : (
+        <h2 className="text-[30px] text-green-600 bg-stone-100">Hello Next</h2>
+      )}
+    </>
+  );
+}
 
 export default function Home() {
   // Local
   function job(): string {
     return myJob;
+  }
+
+  function age(): number {
+    return myAge;
   }
 
   return (
@@ -17,7 +38,12 @@ export default function Home() {
       <div>
         <h2>Nome: {name}</h2>
         <h2>Profissão: {job()}</h2>
+        <h2>Idade: {age()}</h2>
       </div>
+
+      <div className="w-full text-center my-3">{helloWorld(true)}</div>
+      <div className="w-full text-center my-3">{helloWorld(false)}</div>
+      <div className="w-full text-center my-3">{helloWorld(true)}</div>
     </main>
   );
 }
